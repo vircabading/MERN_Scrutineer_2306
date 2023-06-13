b720ec611fb1987621b3e12877bc7f6aa6fc1396
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-06-13 16:59:17 -0500

    implement Subject View

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
+import HomeView from './views/HomeView';
+import SubjectView from './views/SubjectView';
+
+////////////////////////////////////////////////////
+//  ROOT APPLICATION
+////////////////////////////////////////////////////
+
+/**
+ * Root Application
+ * @returns HTML for the Root Application
+ */
+const App = () => {
+
+  // //// OUTPUT ///////////////////////////////////
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch >
+        {/* **** Subject View ******** */}
+        <Route path="/subject/:id">
+          <SubjectView />
+        </Route>
+        {/* **** Home View ******** */}
+        <Route exact path="/">
+          <div className='bg'>
+            <HomeView />
+          </div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
