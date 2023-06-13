95bc28f78590e72694180ec11beebaba0bf3e8cd
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-06-13 12:53:39 -0500

    implement server connection to the database in server.js

--- server/server.js
+++ server/server.js
@@ -12,4 +12,12 @@ const PORT = 8000;
 app.use(express.json(), express.urlencoded({ extended: true }));
 app.use(cors());    // enable Cross Origin Requests in Project
 
+// INITIALIZE SERVER CONNECTION TO DATABASE /////
+require("./config/mongoose.config");
+
+// //// IMPLEMENT ROUTES ////////////////////////
+const AllRoutes = require("./routes/subject.route");
+AllRoutes(app);
+
+// //// EXECUTE SERVER //////////////////////////
 app.listen(PORT, () => console.log(`🦄🦄🦄 The server is all fired up on port ${PORT} 🦄🦄🦄`));
